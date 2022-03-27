f6a1f1e716221f306ec680592a8f9e153a0a06fe
vibelo-vbl <102419144+vibelo-vbl@users.noreply.github.com>
2022-03-27 17:38:20 +0200

    Update new-vpc.tf

--- new-vpc.tf
+++ new-vpc.tf
@@ -5,4 +5,7 @@ provider "aws" {
 module "newvpc" {
   source = "terraform-aws-modules/vpc/aws"
   cidr = "10.7.0.0/16"
+  
+  azs = ["us-west-2a", "us-west-2c"]
+  public_subnets = ["10.7.100.0/24", "10.7.200.0/24"]
 }
